8debb25445b72e97666b6728c1b517955aec332d
Maria <guest@il-g3.msk.21-school.ru>
2025-02-11 18:53:02 +0300

    add SnapKit

--- EasyBuy/SearchVC.swift
+++ EasyBuy/SearchVC.swift
@@ -6,24 +6,50 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class SearchVC: UIViewController {
     
-    var products: [Product] = []
+//    var products: [Product] = []
+    
+    var collectionView: UICollectionView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let apiClient = APIClient()
-                
-        Task {
-            do {
-                products = try await apiClient.getProductList()
-                print(products)
-            } catch {
-                print("Error")
-            }
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 100, height: 100)
+        layout.minimumInteritemSpacing = 10
+        layout.minimumLineSpacing = 10
+        
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        view.addSubview(collectionView)
+        
+//        let apiClient = APIClient()
+//                
+//        Task {
+//            do {
+//                products = try await apiClient.getProductList()
+//                print(products)
+//            } catch {
+//                print("Error")
+//            }
+//        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+            make.leading.trailing.equalToSuperview()
         }
+
+
     }
+    
+    
+    
+    
 
 }
